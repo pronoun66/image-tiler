bed5138f4455694a3cb3c406bc7c70a90d1513bc
Jerry <pronoun66@gmail.com>
2020-04-12 23:06:57 +1000

    initialise page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { App as TodoApp } from 'app/containers/App';
+import { App as ImageTilerApp } from 'app/containers/App';
+import { App as TodoApp } from 'app/containers/TodoApp';
 import { hot } from 'react-hot-loader';
 
 export const App = hot(module)(() => (
   <Switch>
-    <Route path="/" component={TodoApp} />
+    <Route path="/" component={ImageTilerApp} />
+    <Route path="/todo" component={TodoApp} />
   </Switch>
 ));
